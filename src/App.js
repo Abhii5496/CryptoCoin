17729e2dbe267d4321f25a837619d7a8a900b9dc
Abhii5496 <abhishekhsharma2002@gmail.com>
2022-09-29 17:24:47 +0530

    Added homepage coins and search option

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import { ThemeProvider } from './Conntext/ThemeContext'
+import Account from './Pages/Account'
+import Home from './Pages/Home'
+import Signin from './Pages/Signin'
+import SignUp from './Pages/SignUp'
+import axios from 'axios'
 
-function App() {
+const App = () => {
+  const [coins, setCoins] = useState([])
+
+  const url ='https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true&price_change_percentage=1h%2C24h'
+
+  useEffect(()=> {
+    axios.get(url).then((res) => {
+      setCoins(res.data)
+      // console.log(res.data);
+    })
+  },[url])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider>
+     <Navbar />
+     <Routes>
+      <Route path='/' element={<Home coins={coins} />} />
+      <Route path='/signin' element={<Signin/>} />
+      <Route path='/signup' element={<SignUp/>} />
+      <Route path='/account' element={<Account/>} />
+     </Routes>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
